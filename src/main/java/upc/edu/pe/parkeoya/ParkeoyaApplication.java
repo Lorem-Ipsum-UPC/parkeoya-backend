60a517ad5a85d470cc81389dc4f8e27b812ede78
NelsonUPC <164695009+NelsonUPC@users.noreply.github.com>
2025-10-10 21:23:12 -0500

    chore: initial backend project structure

---
+++ src/main/java/upc/edu/pe/parkeoya/ParkeoyaApplication.java
@@ -0,0 +1,13 @@
+package upc.edu.pe.parkeoya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkeoyaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ParkeoyaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/upc/edu/pe/parkeoya/ParkeoyaApplicationTests.java
@@ -0,0 +1,13 @@
+package upc.edu.pe.parkeoya;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkeoyaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
